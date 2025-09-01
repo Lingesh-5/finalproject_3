386b762f33d734e5a3c40a67e4a1e7f4673478d7
Lingesh-5 <lingeshaj1999@gmail.com>
2025-09-01 15:43:23 +0530

    Api base url added

--- src/main/java/com/example/hotelBookingSystem/controllers/HotelWebController.java
+++ src/main/java/com/example/hotelBookingSystem/controllers/HotelWebController.java
@@ -3,12 +3,14 @@ package com.example.hotelBookingSystem.controllers;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 @EnableMethodSecurity
+@CrossOrigin("*")
 public class HotelWebController {
 
     @GetMapping("/hotel")
